4b2b602b1d4f9ee22f6dbfd75fce77a178d0f361
pachocki <101109218+pachocki@users.noreply.github.com>
2022-05-04 23:23:39 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,6 +15,8 @@ function playAudio(url) {
     new Audio(url).play();
   }
 
+  playAudio("./music/batman-theme.mp3")
+
 let monstersArray = ["scarecrow","pingwin","joker"]
 
 function getNewMonster(){
@@ -79,7 +81,6 @@ function endGame(){
 function render(){
     document.getElementById(batman.elementId).innerHTML=batman.getCharacterHtml()
     document.getElementById(monster.elementId).innerHTML=monster.getCharacterHtml()
-    playAudio("./music/batman-theme.mp3")
     
 }
 
