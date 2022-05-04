d7250b07ad50f265503b58838ca48d8d5bf687a6
pachocki <101109218+pachocki@users.noreply.github.com>
2022-05-04 23:11:44 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -86,7 +86,7 @@ function render(){
 document.getElementById("atack").addEventListener("click" , attack)
 const batmanSound = new Audio('./music/batman.mp3');
 const jokerSound = new Audio('./music/joker.mp3');
-const bodyMusic = new Audio('./music/batman-theme.mp3')
+const bodyMusic = new Audio('./music/batman-game-music.mp3')
 document.querySelector("#hero"). addEventListener ("mouseover", function() {
     batmanSound.play();
 });
