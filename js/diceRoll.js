93c4033dd0d591866bc7c641a1be11ec7994d03c
pachocki <101109218+pachocki@users.noreply.github.com>
2022-05-05 12:46:24 +0200

    Update diceRoll.js

--- js/diceRoll.js
+++ js/diceRoll.js
@@ -1,15 +1,17 @@
 function getDiceRollArray(diceCount){
-    return new Array(diceCount).fill(0).map(function(){
-        return Math.floor(Math.random()*6)+1
-    })
+    return new Array(diceCount).fill(0).map(()=>
+    Math.floor(Math.random()*6)+1)
 }
 
-function getDicePlaceHolderHtml(diceCount){
-    return new Array(diceCount).fill(0).map(function(){
-        return `<div class="placeholder-dice"></div>`
-    }).join(" ")
-}
 const getPercentage = (remainingHealth, maximumHealth) => 
     (100 * remainingHealth) / maximumHealth
+
+
+function getDicePlaceHolderHtml(diceCount){
+    return new Array(diceCount).fill(0).map(()=>
+        `<div class="placeholder-dice"></div>`
+    ).join(" ")
+}
+
 
 export {getDiceRollArray,getDicePlaceHolderHtml,getPercentage}
